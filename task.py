73535583018027ce1c8e261651188f80eb7bdbd9
Mikolaj  Szczepanski <4szczepanski@taurus.fis.agh.edu.pl>
2018-03-12 19:26:42 +0100

    .

--- task.py
+++ task.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Class diary  
 #
 # Create program for handling lesson scores.
@@ -21,3 +22,54 @@
 #
 #Your program must be runnable with command "python task.py".
 #Show some usecases of your library in the code (print some things)
+
+class Student:
+	def __init__(self, firstname, lastname):
+		self.firstname = firstname
+		self.lastname = lastname
+	def __str__(self):
+		return 'Student: ' + str(self.firstname) + ' ' + str(self.lastname)
+
+class Class:
+	def __init__(self, title):
+		self.title = title
+		self.students = []
+
+	def add_student(self,student):
+		self.students.append((student,[]))
+
+	def get_students(self):
+		return map(lambda s: s[0], self.students)
+
+	def add_score_for_student(self,student,score):
+		student_index = self.get_students().index(student)
+		self.students[student_index][1].append(score)
+
+	def __str__(self):
+		return_str = 'Class ' + self.title
+		for student in self.students:
+			return_str = return_str + '\n' + str(student[0]) + ' scores: ' + str(student[1])
+		return return_str
+	
+		
+def main():
+	test_student_1 = Student("Jan","Kowalski")
+	test_student_2 = Student("Iwona","Kowalska")
+
+	python_class = Class("Python")
+
+	python_class.add_student(test_student_1)
+	python_class.add_student(test_student_2)
+
+	python_class.add_score_for_student(test_student_1, 3.0)
+	python_class.add_score_for_student(test_student_2, 3.5)
+
+	print(str(python_class))
+	print('main')
+
+
+print('start')
+main()
+
+
+
